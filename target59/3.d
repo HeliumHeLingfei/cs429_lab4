6aa4f061c97e3d41b4d5231cdbc785cdb1ee0b46
Lingfei He <heliumhe@cs.utexas.edu>
2018-10-27 04:11:17 -0500

    third

---
+++ target59/3.d
@@ -0,0 +1,11 @@
+
+3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 83 ec 21          	sub    $0x21,%rsp
+   4:	68 68 1a 40 00       	pushq  $0x401a68
+   9:	48 c7 c7 28 64 67 55 	mov    $0x55676428,%rdi
+  10:	c3                   	retq   
---
+++ target59/3.s
@@ -0,0 +1,4 @@
+sub	$0x21, %rsp
+pushq	$0x0000000000401a68
+mov	$0x55676428, %rdi
+ret
