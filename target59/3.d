567aff257735cb52fd819268e084320ea76043dc
Lingfei He <heliumhe@cs.utexas.edu>
2018-11-06 16:05:01 -0600

    phase 4

--- target59/3.d
+++ target59/3.d
@@ -7,5 +7,5 @@ Disassembly of section .text:
 0000000000000000 <.text>:
    0:	48 83 ec 21          	sub    $0x21,%rsp
    4:	68 68 1a 40 00       	pushq  $0x401a68
-   9:	48 c7 c7 28 64 67 55 	mov    $0x55676428,%rdi
+   9:	48 c7 c7 27 64 67 55 	mov    $0x55676427 ,%rdi
   10:	c3                   	retq   
